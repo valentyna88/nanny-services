6d24f3eaea3d854ee4d4855821a9e807fe4a38cf
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-05 16:47:37 +0200

    feat(router): implement routing with react-router for main pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.module.css';
+import Layout from '../Layout';
+import Home from '../../pages/Home/Home';
+import Nannies from '../../pages/Nannies/Nannies';
+import Favorites from '../../pages/Favorites/Favorites';
+import NotFound from '../../pages/NotFound/NotFound';
 
 const App = () => {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/nannies" element={<Nannies />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import Loader from './Loader/Loader';
+import Header from './Header/Header';
+
 const Layout = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
+    </>
+  );
 };
 
 export default Layout;
