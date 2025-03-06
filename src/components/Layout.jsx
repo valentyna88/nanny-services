3957ddfbd34042f4b3cadd67afe3956184c313bf
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-07 00:37:04 +0200

    feat(ui): add Container component and install modern-normalize

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,15 +2,18 @@ import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import Loader from './Loader/Loader';
 import Header from './Header/Header';
+import Container from './ui/Container/Container';
 
 const Layout = () => {
   return (
     <>
       <Header />
       <main>
-        <Suspense fallback={<Loader />}>
-          <Outlet />
-        </Suspense>
+        <Container>
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+        </Container>
       </main>
     </>
   );
