28557f7ac71bce1bd7723b7a2969aebe461e700f
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-07 15:02:06 +0200

    feat: add Toaster notification system

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
+import { Toaster } from 'react-hot-toast';
 import Loader from './Loader/Loader';
 import Header from './Header/Header';
 import Container from './ui/Container/Container';
@@ -7,7 +8,16 @@ import Container from './ui/Container/Container';
 const Layout = () => {
   return (
     <>
-      <Header />
+      <Header />{' '}
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            padding: '16px',
+            marginTop: '80px',
+          },
+        }}
+      />
       <main>
         <Container>
           <Suspense fallback={<Loader />}>
