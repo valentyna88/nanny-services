23d153a04a146f3e66a3ca10b9242e11a154c4ce
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-07 18:45:07 +0200

    feat: add NotFoundPage with styles

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,19 @@
-const NotFound = () => {
-  return <div>NotFound</div>;
+import { Link } from 'react-router-dom';
+import Container from '../../components/ui/Container/Container';
+import css from './NotFoundPage.module.css';
+
+const NotFoundPage = () => {
+  return (
+    <Container>
+      <div className={css.wrapper}>
+        <h1 className={css.title}>404</h1>
+        <p className={css.text}>Oops! Page not found.</p>
+        <Link to="/" className={css.link}>
+          Go to Home
+        </Link>
+      </div>
+    </Container>
+  );
 };
 
-export default NotFound;
+export default NotFoundPage;
