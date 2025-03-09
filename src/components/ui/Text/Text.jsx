6cd8eac8430a589343640f3ba9b756c8325a0ab0
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-09 17:18:49 +0200

    feat: add global Text component with styles

---
+++ src/components/ui/Text/Text.jsx
@@ -0,0 +1,12 @@
+import clsx from 'clsx';
+import css from './Text.module.css';
+
+const Text = ({ children, variant = 'default', className }) => {
+  return (
+    <p className={clsx(css.text, variant && css[variant], className)}>
+      {children}
+    </p>
+  );
+};
+
+export default Text;
