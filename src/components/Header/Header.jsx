80d32376a7c2b80823b1515aaab74d6dc918cecc
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-10 23:20:34 +0200

    feat: add Header markup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,34 @@
+import { Link, NavLink } from 'react-router-dom';
+import css from './Header.module.css';
+import clsx from 'clsx';
+
 const Header = () => {
-  return <div>Header</div>;
+  const buildLinkClass = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
+  return (
+    <header className={css.header}>
+      <Link to="/" className={clsx(css.link, css.logo)}>
+        Nanny.Services
+      </Link>
+      <nav className={css.nav}>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/nannies" className={buildLinkClass}>
+          Nannies
+        </NavLink>
+      </nav>
+      <div>
+        <Link to="/login" className={css.login}>
+          Log In
+        </Link>
+        <Link to="/register" className={css.register}>
+          Registration
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
