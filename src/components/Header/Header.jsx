19188f6783d78993cdce8561f994fa868b42db98
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-11 13:58:17 +0200

    feat: add HomePage styles, update Header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,24 +9,26 @@ const Header = () => {
   };
   return (
     <header className={css.header}>
-      <Link to="/" className={css.logo}>
-        Nanny.Services
-      </Link>
-      <nav className={css.nav}>
-        <NavLink to="/" className={buildLinkClass}>
-          Home
-        </NavLink>
-        <NavLink to="/nannies" className={buildLinkClass}>
-          Nannies
-        </NavLink>
-      </nav>
-      <div className={css.authButtons}>
-        <Button type="button" variant="outline" to="/login" as={NavLink}>
-          Log In
-        </Button>
-        <Button type="button" variant="filled" to="/register" as={NavLink}>
-          Registration
-        </Button>
+      <div className={css.wrapper}>
+        <Link to="/" className={css.logo}>
+          Nanny.Services
+        </Link>
+        <nav className={css.nav}>
+          <NavLink to="/" className={buildLinkClass}>
+            Home
+          </NavLink>
+          <NavLink to="/nannies" className={buildLinkClass}>
+            Nannies
+          </NavLink>
+        </nav>
+        <div className={css.authNav}>
+          <Button type="button" variant="outline" to="/login" as={NavLink}>
+            Log In
+          </Button>
+          <Button type="button" variant="filled" to="/register" as={NavLink}>
+            Registration
+          </Button>
+        </div>
       </div>
     </header>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,27 @@
-const Home = () => {
+import { NavLink } from 'react-router-dom';
+import Button from '../../components/ui/Button/Button';
+import Text from '../../components/ui/Text/Text';
+import Title from '../../components/ui/Title/Title';
+import css from './HomePage.module.css';
+
+const HomePage = () => {
   return (
-    <div>
-      <h1>Make Life Easier for the Family:</h1>
+    <div className={css.wrapper}>
+      <div className={css.leftBlock}>
+        <Title variant="main">Make Life Easier for the Family:</Title>
+        <Text variant="home">Find Babysitters Online for All Occasions</Text>
+        <Button type="button" variant="started" to="/nannies" as={NavLink}>
+          Get started
+        </Button>
+      </div>
+      <div className={css.rightBlock}>
+        <div className={css.checkBlock}>
+          <Text variant="light">Experienced nannies</Text>
+          <Text variant="check">15,000</Text>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default HomePage;
