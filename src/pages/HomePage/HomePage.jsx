b5867d3614c64899018c5b159f485dc054c7f01f
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-21 23:27:21 +0200

    chore: update HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,22 +2,43 @@ import { NavLink } from 'react-router-dom';
 import Button from '../../components/ui/Button/Button';
 import Text from '../../components/ui/Text/Text';
 import Title from '../../components/ui/Title/Title';
+import sprite from '../../assets/sprite.svg';
 import css from './HomePage.module.css';
 
 const HomePage = () => {
   return (
     <div className={css.wrapper}>
       <div className={css.leftBlock}>
-        <Title variant="main">Make Life Easier for the Family:</Title>
-        <Text variant="home">Find Babysitters Online for All Occasions</Text>
-        <Button type="button" variant="started" to="/nannies" as={NavLink}>
+        <Title variant="main" className={css.title}>
+          Make Life Easier for the Family:
+        </Title>
+        <Text variant="home" className={css.text}>
+          Find Babysitters Online for All Occasions
+        </Text>
+        <Button
+          type="button"
+          variant="started"
+          to="/nannies"
+          as={NavLink}
+          className={css.btn}
+        >
           Get started
+          <svg width={15} height={17} className={css.icon}>
+            <use xlinkHref={`${sprite}#icon-Arrow16`}></use>
+          </svg>
         </Button>
       </div>
       <div className={css.rightBlock}>
         <div className={css.checkBlock}>
-          <Text variant="light">Experienced nannies</Text>
-          <Text variant="check">15,000</Text>
+          <div className={css.checkIcon}>
+            <svg width={20} height={16}>
+              <use xlinkHref={`${sprite}#icon-check`}></use>
+            </svg>
+          </div>
+          <div className={css.checkText}>
+            <Text variant="light">Experienced nannies</Text>
+            <Text variant="check">15,000</Text>
+          </div>
         </div>
       </div>
     </div>
