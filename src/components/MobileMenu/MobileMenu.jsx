521e0fbb176c95a54fbf7c55b13c2ecdc44ce2d2
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-24 21:14:17 +0200

    chore(MobileMenu): update file

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -2,13 +2,8 @@ import { MdClose } from 'react-icons/md';
 import Navigation from '../Navigation/Navigation';
 import AuthBtn from '../AuthBtn/AuthBtn';
 import css from './MobileMenu.module.css';
-import clsx from 'clsx';
 
 const MobileMenu = ({ closeMenu }) => {
-  const buildLinkClass = ({ isActive }) => {
-    return clsx(css.link, isActive && css.active);
-  };
-
   return (
     <div className={css.menu}>
       <button className={css.closeButton} type="button" onClick={closeMenu}>
