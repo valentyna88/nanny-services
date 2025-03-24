386f718473624d44c634a64015fda25b0887897d
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-24 11:02:08 +0200

    feat: add MobileMenu logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import MobileMenu from '../MobileMenu/MobileMenu';
 import Navigation from '../Navigation/Navigation';
 import AuthBtn from '../AuthBtn/AuthBtn';
 import css from './Header.module.css';
 import clsx from 'clsx';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation();
 
   const isHomePage = location.pathname === '/';
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
 
   return (
     <header
@@ -15,6 +27,17 @@ const Header = () => {
         [css.homeHeader]: isHomePage,
       })}
     >
+      {/* Mobile Menu */}
+      <div className={css.mobileHeader}>
+        <Link to="/" className={css.logo}>
+          Nanny.Services
+        </Link>
+        <button className={css.hamburger} type="button" onClick={toggleMenu}>
+          <RxHamburgerMenu />
+        </button>
+      </div>
+
+      {/* Desktop Navigation */}
       <div className={css.wrapper}>
         <Link to="/" className={css.logo}>
           Nanny.Services
@@ -22,6 +45,7 @@ const Header = () => {
         <Navigation />
         <AuthBtn />
       </div>
+      {menuOpen && <MobileMenu closeMenu={closeMenu} />}
     </header>
   );
 };
