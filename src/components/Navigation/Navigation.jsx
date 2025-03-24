6f26ffa55ef8eeadda2aa913f04b182e38826926
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-24 11:04:41 +0200

    feat: add closeMenu to Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,17 +2,17 @@ import { NavLink } from 'react-router-dom';
 import clsx from 'clsx';
 import css from './Navigation.module.css';
 
-const Navigation = () => {
+const Navigation = ({ closeMenu }) => {
   const buildLinkClass = ({ isActive }) => {
     return clsx(css.link, isActive && css.active);
   };
 
   return (
     <nav className={css.nav}>
-      <NavLink to="/" className={buildLinkClass}>
+      <NavLink to="/" className={buildLinkClass} onClick={closeMenu}>
         Home
       </NavLink>
-      <NavLink to="/nannies" className={buildLinkClass}>
+      <NavLink to="/nannies" className={buildLinkClass} onClick={closeMenu}>
         Nannies
       </NavLink>
     </nav>
