1e7efca0a6435f95ecea3abec252dad744d442b4
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-03-26 16:03:00 +0200

    feat(NanniesPage): fetch and display nannies from Firebase

--- src/pages/NanniesPage/NanniesPage.jsx
+++ src/pages/NanniesPage/NanniesPage.jsx
@@ -1,9 +1,29 @@
-const Nannies = () => {
-  return (
-    <div>
-      <h1>Our Nannies</h1>
-    </div>
-  );
+import { useEffect, useState } from 'react';
+import { ref, get } from 'firebase/database';
+import { database } from '../../firebase';
+import NannyList from '../../components/NannyList/NannyList';
+
+const NanniesPage = () => {
+  const [nannies, setNannies] = useState([]);
+
+  useEffect(() => {
+    const fetchNannies = async () => {
+      try {
+        const nanniesRef = ref(database, 'babysitters');
+        const snapshot = await get(nanniesRef);
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          const formattedData = Object.values(data);
+          setNannies(formattedData);
+        }
+      } catch (error) {
+        console.error('Loading failed:', error);
+      }
+    };
+    fetchNannies();
+  }, []);
+
+  return <NannyList nannies={nannies} />;
 };
 
-export default Nannies;
+export default NanniesPage;
