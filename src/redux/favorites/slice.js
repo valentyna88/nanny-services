44f858e8d58d94f95e33a9014a9962b16ebd460c
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-01 01:55:58 +0300

    feat: add favorites slice

---
+++ src/redux/favorites/slice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  favorites: [],
+};
+
+const favoritesSlice = createSlice({
+  name: 'favorites',
+  initialState,
+  reducers: {
+    addFavorite(state, action) {
+      state.favorites.push(action.payload);
+    },
+    removeFavorite(state, action) {
+      state.favorites = state.favorites.filter(
+        nanny => nanny.id !== action.payload
+      );
+    },
+  },
+});
+
+export const { addFavorite, removeFavorite } = favoritesSlice.actions;
+export default favoritesSlice.reducer;
