efafb6d0d6a5d5d607e9caec71d84a8d33a79163
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-01 01:48:28 +0300

    feat: add store

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './redux/store';
 import App from './components/App/App';
 import 'modern-normalize';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </Provider>
 );
