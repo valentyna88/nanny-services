0624ea7c4f90b7a2da21616002533350fb98cf7b
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-01 12:24:28 +0300

    feat: add Redux selectors for nannies

---
+++ src/redux/nannies/selectors.js
@@ -0,0 +1,3 @@
+export const selectNannies = state => state.nannies.items;
+export const selectIsLoading = state => state.nannies.isLoading;
+export const selectError = state => state.nannies.error;
