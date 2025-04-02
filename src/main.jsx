dcd695d629647e46d6a1946c80df41a64d162549
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-02 16:02:27 +0300

    feat: add redux-persist for persistent favorites state

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import App from './components/App/App';
 import 'modern-normalize';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StrictMode>
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit';
-import nanniesReducer from './nannies/slice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { favoritesReducer } from './favorites/slice';
+import nanniesReducer from './nannies/slice';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'favorites',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, favoritesReducer);
 
 export const store = configureStore({
   reducer: {
     nannies: nanniesReducer,
-    favorites: favoritesReducer,
+    favorites: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
