221e6a0639c070367bcda7f79c9977c50ff228c5
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-03 09:07:25 +0300

    feat: add Load more button

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,5 +1,14 @@
-const LoadMoreBtn = () => {
-  return <div>LoadMoreBtn</div>;
+import Button from '../ui/Button/Button';
+import css from './LoadMoreBtn.module.css';
+
+const LoadMoreBtn = ({ onClick }) => {
+  return (
+    <div className={css.btnWrapper}>
+      <Button type="button" variant="filled" onClick={onClick}>
+        Load more
+      </Button>
+    </div>
+  );
 };
 
 export default LoadMoreBtn;
