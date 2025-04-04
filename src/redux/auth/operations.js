c7b67b354264cafc0e1945f9a4aa42b423c456c2
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-04 14:11:36 +0300

    fix: fix auth operations

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,3 +1,4 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -5,53 +6,62 @@ import {
   onAuthStateChanged,
 } from 'firebase/auth';
 import { auth } from '../../firebase';
-import { setUser, clearUser } from './slice';
 
-export const register =
-  ({ email, password }) =>
-  async dispatch => {
+// РЕЄСТРАЦІЯ
+export const register = createAsyncThunk(
+  'auth/register',
+  async ({ email, password }, thunkAPI) => {
     try {
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       );
-      dispatch(setUser(userCredential.user));
+      return userCredential.user;
     } catch (error) {
-      console.error('Register error:', error.message);
+      return thunkAPI.rejectWithValue(error.message);
     }
-  };
+  }
+);
 
-export const login =
-  ({ email, password }) =>
-  async dispatch => {
+// ЛОГІН
+export const login = createAsyncThunk(
+  'auth/login',
+  async ({ email, password }, thunkAPI) => {
     try {
       const userCredential = await signInWithEmailAndPassword(
         auth,
         email,
         password
       );
-      dispatch(setUser(userCredential.user));
+      return userCredential.user;
     } catch (error) {
-      console.error('Login error:', error.message);
+      return thunkAPI.rejectWithValue(error.message);
     }
-  };
+  }
+);
 
-export const logout = () => async dispatch => {
+// ЛОГАУТ
+export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
     await signOut(auth);
-    dispatch(clearUser());
   } catch (error) {
-    console.error('Logout error:', error.message);
+    return thunkAPI.rejectWithValue(error.message);
   }
-};
+});
 
-export const getCurrentUser = () => dispatch => {
-  onAuthStateChanged(auth, user => {
-    if (user) {
-      dispatch(setUser(user));
-    } else {
-      dispatch(clearUser());
-    }
-  });
-};
+// ОТРИМАННЯ ПОТУЖНОГО КОРИСТУВАЧА
+export const getCurrentUser = createAsyncThunk(
+  'auth/getCurrentUser',
+  async (_, thunkAPI) => {
+    return new Promise((resolve, reject) => {
+      onAuthStateChanged(auth, user => {
+        if (user) {
+          resolve(user);
+        } else {
+          reject('No user found');
+        }
+      });
+    });
+  }
+);
