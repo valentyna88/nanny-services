4cb32ad19d6640729073b99ddf671f6dc4ceb0ae
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-04 14:13:29 +0300

    refactor: Move handlePending and handleRejected logic to separate functions

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,9 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getCurrentUser, login, logout, register } from './operations';
+import { register, login, logout, getCurrentUser } from './operations';
+
+const handlePending = state => {
+  state.isLoading = true;
+  state.error = null;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
 
 const initialState = {
   user: null,
   isLoggedIn: false,
+  isLoading: false,
+  error: null,
+  favorites: [],
 };
 
 const authSlice = createSlice({
@@ -17,36 +30,27 @@ const authSlice = createSlice({
     clearUser: state => {
       state.user = null;
       state.isLoggedIn = false;
+      state.favorites = [];
     },
   },
   extraReducers: builder => {
     builder
-      .addCase(register.pending, state => {
-        state.isFetching = true;
-        state.error = null;
-      })
+      .addCase(register.pending, handlePending)
       .addCase(register.fulfilled, (state, action) => {
-        state.isFetching = false;
+        state.isLoading = false;
         state.isLoggedIn = true;
         state.user = action.payload;
       })
-      .addCase(register.rejected, (state, action) => {
-        state.isFetching = false;
-        state.error = action.payload;
-      })
-      .addCase(login.pending, state => {
-        state.isFetching = true;
-        state.error = null;
-      })
+      .addCase(register.rejected, handleRejected)
+
+      .addCase(login.pending, handlePending)
       .addCase(login.fulfilled, (state, action) => {
-        state.isFetching = false;
+        state.isLoading = false;
         state.isLoggedIn = true;
         state.user = action.payload;
       })
-      .addCase(login.rejected, (state, action) => {
-        state.isFetching = false;
-        state.error = action.payload;
-      })
+      .addCase(login.rejected, handleRejected)
+
       .addCase(logout.fulfilled, state => {
         state.user = null;
         state.isLoggedIn = false;
