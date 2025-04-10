b326e2a3d237ee0a64f58ff77d85c7604fb7e50b
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-10 20:02:24 +0300

    refactor: moved calculateAge function to utils folder

--- src/components/NannyCard/NannyCard.jsx
+++ src/components/NannyCard/NannyCard.jsx
@@ -5,28 +5,12 @@ import { addFavorite, removeFavorite } from '../../redux/favorites/slice';
 import { openModal } from '../../redux/modal/slice';
 import { toast } from 'react-hot-toast';
 import { getAuth } from 'firebase/auth';
+import { calculateAge } from '../../utils/calculateAge';
 import Text from '../ui/Text/Text';
 import Title from '../ui/Title/Title';
 import Button from '../ui/Button/Button';
 import sprite from '../../assets/sprite.svg';
 import css from './NannyCard.module.css';
-
-const calculateAge = birthday => {
-  if (!birthday) return 'N/A';
-
-  const birthDate = new Date(birthday);
-  const today = new Date();
-
-  let age = today.getFullYear() - birthDate.getFullYear();
-  const monthDiff = today.getMonth() - birthDate.getMonth();
-  const dayDiff = today.getDate() - birthDate.getDate();
-
-  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
-    age--;
-  }
-
-  return age;
-};
 
 const NannyCard = ({ nanny }) => {
   const dispatch = useDispatch();
---
+++ src/utils/calculateAge.js
@@ -0,0 +1,16 @@
+export const calculateAge = birthday => {
+  if (!birthday) return 'N/A';
+
+  const birthDate = new Date(birthday);
+  const today = new Date();
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  const dayDiff = today.getDate() - birthDate.getDate();
+
+  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+    age--;
+  }
+
+  return age;
+};
