b902f395ff38f239cfbe90ebdbb0af1d6b3ef0e5
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-10 03:06:11 +0300

    feat: add TimePicker component

---
+++ src/components/TimePicker/TimePicker.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import sprite from '../../assets/sprite.svg';
+import css from './TimePicker.module.css';
+
+const times = ['09:00', '09:30', '10:00', '10:30'];
+
+const TimePicker = ({ register, setValue, setError, errors }) => {
+  const [selectedTime, setSelectedTime] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelectTime = time => {
+    setSelectedTime(time);
+    setValue('time', time);
+    setError('time', { type: 'manual', message: '' });
+    setIsOpen(false);
+  };
+
+  return (
+    <div className={css.inputBox}>
+      <div className={css.customSelect} onClick={() => setIsOpen(!isOpen)}>
+        {selectedTime || '00:00'}
+        <button type="button" className={css.clockButton}>
+          <svg className={css.icon}>
+            <use href={`${sprite}#icon-clock`}></use>
+          </svg>
+        </button>
+      </div>
+      {isOpen && (
+        <div className={css.dropdown}>
+          <p className={css.dropdownLabel}>Meeting time</p>
+          <ul>
+            {times.map(time => (
+              <li
+                key={time}
+                className={css.option}
+                onClick={() => handleSelectTime(time)}
+              >
+                {time}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <input type="hidden" {...register('time')} value={selectedTime} />
+      {errors.time?.message && (
+        <span className={css.error}>{errors.time.message}</span>
+      )}
+    </div>
+  );
+};
+
+export default TimePicker;
